daed28bac97ef7135ad0cc8ae42aca256c69c032
SW.Park <SW.Park@SW-GRAM>
2022-10-31 12:58:41 +0900

    hi world 메시지 추가 추가

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,8 +1,8 @@
 
 public class HelloWorld {
 	public static void main(String[] orgs) {
-		System.out.println("Hello World");
-		System.out.println("See you");
+		System.out.println("Hi World");
+		System.out.println("See you again");
 	}
 
 }
